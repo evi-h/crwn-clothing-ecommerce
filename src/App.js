4bcd4a3a344502ecc817b62b81e038eb61d6c7bb
Evia-H <evi1020@gmail.com>
2021-05-21 19:38:03 -0700

    created homepage, added node-sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './homepage.component'
 
 function App() {
   return (
     <div className="App">
-        Hello World!!!
+        <HomePage/>
     </div>
   );
 }
